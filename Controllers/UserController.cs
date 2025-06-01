2ef3ac0c8d5b4443c0f52aa9b0738705da0c9620
Louis Atkinson <louisatkinson.la@gmail.com>
2025-06-01 10:33:28 +0100

    add additional user controller methods

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
 using api.Data;
 using api.Dtos;
 using api.Models;
